cc81e9b517747816fa04fc372a58b24bac991da6
whyrupesh <srupeshkumar800@gmail.com>
2026-02-14 09:55:56 +0530

    basic deployable is added4

--- app/main.py
+++ app/main.py
@@ -9,6 +9,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/hello")
+def hello():
+    return {"message": "Hello from the /hello endpoint!"}
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
